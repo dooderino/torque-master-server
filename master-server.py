d8a5ba14fc4d9d99f13e71e186d3837203dd2a4e
Joe Spataro <jspataro@gmail.com>
2022-11-06 11:47:25 -0800

    Remove bitstream & numpy dependency: use construct

--- master-server.py
+++ master-server.py
@@ -1,7 +1,14 @@
 import socket
-from bitstream import BitStream
-from numpy import *
+#from numpy import *
 from enum import Enum
+from typing import NamedTuple, List
+from datetime import *
+import logging
+from construct import *
+
+localIP     = "127.0.0.1"
+localPort   = 20001
+bufferSize  = 1024
 
 class PacketTypes(Enum):
     MasterServerGameTypesRequest  = 2
@@ -27,115 +34,183 @@ class PacketTypes(Enum):
     MasterServerChallenge            = 42
     MasterServerExtendedListRequest  = 44
 
+class IPAddress(NamedTuple):
+    Address: str
+    Port: Int32sb
+
+class ServerInfo(NamedTuple):
+    IPAddress: IPAddress
+    GameType: str
+    MissionType: str
+    MaxPlayers: Int32sb
+    Region: Int32ub
+    Version: Int32ub
+    Flags: Int16ub
+    BotCount: Int16ub
+    CPUSpeed: Int32ub
+    PlayerCount: Int32sb
+    PlayerList: List[int] = []
+    
+def Session(NamedTuple):
+    SessionID: uint16
+    Key: uint16
+    CreatedDate: timedelta
+    Heartbeat: timedelta
+    
+class Sessions(object):
+    def __init__(self) -> None:
+        self.sessions = {}
+        self.creation_time= datetime.now()
+        self.logger = logging.getLogger('Sessions')
+        
+    def has_session(self, ip_address):
+        return ip_address in self.sessions
+    
+    def heartbeat(self, ip_address):
+        result = False
+        if self.has_session(ip_address):
+            self.sessions[ip_address].Heartbeat = self.creation_time - datetime.now()
+            result = True
+        return result
+    
+    def get_session(self, ip_address):
+        return self.sessions.get(ip_address)
+    
+    def create_session(self, ip_address):
+        if self.has_session(ip_address):
+            raise Exception("Cannot creation session for address {address}".format(address= ip_address))
+        
+        uint16_min = iinfo(uint16).min
+        uint16_max = iinfo(uint16).max
+        
+        new_session = Session(
+            SessionID = random.randint(uint16_min, uint16_max, dtype=uint16),
+            Key = random.randint(uint16_min, uint16_max, dtype=uint16),
+            CreatedDate = self.creation_time - datetime.now(),
+            Heartbeat = self.creation_time - datetime.now()
+        )
+        
+        self.logger.info("{addr} Session: {id}, Key: {key}".format(addr=ip_address, id=new_session.SessionID, key=new_session.Key))
+
+        self.sessions[ip_address] = new_session
+        
+        return new_session
 
 def process_message(message):
-    stream = BitStream(message)
-    packet_type = stream.read(uint8, 1).item(0) 
-    process_packet_type(packet_type, stream)
+    #stream = BitStream(message)
+    #packet_type = stream.read(uint8, 1).item(0)
+    packet_type= Byte.parse(message[:1]) 
+    print("Packet type: ", packet_type)
+    process_packet_type(packet_type, message)
 
 def process_packet_type(packet_type, stream):
-    if packet_type == PacketTypes.MasterServerGameTypesRequest.value:
+    if packet_type == PacketTypes.MasterServerGameTypesRequest:
         print("Received MasterServerGameTypesRequest packet...")
-    if packet_type == PacketTypes.MasterServerGameTypesResponse.value:
+    if packet_type == PacketTypes.MasterServerGameTypesResponse:
         print("Received MasterServerGameTypesResponse packet...")
-    if packet_type == PacketTypes.MasterServerListRequest.value:
-        print("Received MasterServerGameTypesResponse packet...")
+    if packet_type == PacketTypes.MasterServerListRequest:
+        print("Received MasterServerListRequest packet...")
         process_master_server_list_request(stream)
-    if packet_type == PacketTypes.MasterServerListResponse.value:
+    if packet_type == PacketTypes.MasterServerListResponse:
         print("Received MasterServerListResponse packet...")
-    if packet_type == PacketTypes.GameMasterInfoRequest.value:
+    if packet_type == PacketTypes.GameMasterInfoRequest:
         print("Received GameMasterInfoRequest packet...")
-    if packet_type == PacketTypes.GameMasterInfoResponse.value:
+    if packet_type == PacketTypes.GameMasterInfoResponse:
         print("Received GameMasterInfoResponse packet...")
-    if packet_type == PacketTypes.GamePingRequest.value:
+    if packet_type == PacketTypes.GamePingRequest:
         print("Received GamePingRequest packet...")
-    if packet_type == PacketTypes.GamePingResponse.value:
+    if packet_type == PacketTypes.GamePingResponse:
         print("Received GamePingResponse packet...")
-    if packet_type == PacketTypes.GameInfoRequest.value:
+    if packet_type == PacketTypes.GameInfoRequest:
         print("Received GameInfoRequest packet...")
-    if packet_type == PacketTypes.GameInfoResponse.value:
+    if packet_type == PacketTypes.GameInfoResponse:
         print("Received GameInfoResponse packet...")
-    if packet_type == PacketTypes.GameHeartbeat.value:
+    if packet_type == PacketTypes.GameHeartbeat:
         print("Received GameHeartbeat packet...")
         process_game_heartbeat(stream)
-    if packet_type == PacketTypes.GGCPacket.value:
+    if packet_type == PacketTypes.GGCPacket:
         print("Received GGCPacket packet...")
-    if packet_type == PacketTypes.ConnectChallengeRequest.value:
+    if packet_type == PacketTypes.ConnectChallengeRequest:
         print("Received ConnectChallengeRequest packet...")
     if packet_type == PacketTypes.ConnectChallengeReject:
         print("Received ConnectChallengeReject packet...")
-    if packet_type == PacketTypes.ConnectChallengeResponse.value:
+    if packet_type == PacketTypes.ConnectChallengeResponse:
         print("Received ConnectChallengeResponse packet...")
-    if packet_type == PacketTypes.ConnectRequest.value:
+    if packet_type == PacketTypes.ConnectRequest:
         print("Received ConnectRequest packet...")
-    if packet_type == PacketTypes.ConnectReject.value:
+    if packet_type == PacketTypes.ConnectReject:
         print("Received ConnectReject packet...")
-    if packet_type == PacketTypes.ConnectAccept.value:
+    if packet_type == PacketTypes.ConnectAccept:
         print("Received ConnectAccept packet...")
-    if packet_type == PacketTypes.Disconnect.value:
+    if packet_type == PacketTypes.Disconnect:
         print("Received Disconnect packet...")
-    if packet_type == PacketTypes.MasterServerExtendedListResponse.value:
+    if packet_type == PacketTypes.MasterServerExtendedListResponse:
         print("Received MasterServerExtendedListResponse packet...")
-    if packet_type == PacketTypes.MasterServerChallenge.value:
+    if packet_type == PacketTypes.MasterServerChallenge:
         print("Received MasterServerChallenge packet...")
-    if packet_type == PacketTypes.MasterServerExtendedListRequest.value:
+    if packet_type == PacketTypes.MasterServerExtendedListRequest:
         print("Received MasterServerExtendedListRequest packet...")
         
 def process_master_server_list_request(stream):
-    query_flags= stream.read(uint8, 1)
-    sequence_number= stream.read(int32, 1)
-    packet_index= stream.read(uint8, 1)
-    game_type_length= stream.read(uint8, 1)
-    game_type= stream.read(bytes, game_type_length)
-    mission_type_length= stream.read(uint8, 1)
-    mission_type= stream.read(bytes, mission_type_length)
-    min_players= stream.read(uint8, 1)
-    max_players= stream.read(uint8, 1)
-    version= stream.read(uint32, 1)
-    filter_flags= stream.read(uint8, 1)
-    max_bots= stream.read(uint8, 1)
-    min_cpu= stream.read(uint16, 1)
-    buddy_count= stream.read(uint8, 1)
+    game_type_length = Struct(
+        Padding(7),
+        "length" / Int8ub,
+    )
+
+    gtl = game_type_length.parse(stream)
     
-    print(query_flags, 
-        sequence_number, 
-        packet_index, 
-        game_type, 
-        mission_type, 
-        min_players, 
-        max_players, 
-        version, 
-        filter_flags, 
-        max_bots,
-        min_cpu,
-        buddy_count)
+    mission_type_length = Struct(
+        Padding(8 + gtl.length),
+        "length" / Int8ub,
+    )
+
+    mtl = mission_type_length.parse(stream)
+    
+    format = Struct(
+        "packet_type" / Int8ub, 
+        "flags" / Int8ub,       
+        "session" / Int16ub,    
+        "key" / Int16ub,        
+        "pad" / Int8ub,         
+        "game_type_length" / Int8ub, 
+        "game_type" / PaddedString(gtl.length, "ascii"),
+        "mission_type_length" / Int8ub,
+        "mission_type" / PaddedString(mtl.length, "ascii"),
+        "min_players" / Int8ub,
+        "max_players" / Int8ub,
+        "version" / Int32ub,
+        "filter_flags" / Int8ub,
+        "max_bots" / Int8ub,
+        "min_cpu" / Int16ub,
+        "buddy_count" / Int8ub,
+    )
+
+    result = format.parse(stream)
+    print(result)
 
 def process_game_heartbeat(stream):
-    query_flags= stream.read(uint8, 1)
-    session = stream.read(uint32, 1)
-    print(query_flags, session)
+    format = Struct(
+        "packet_type" / Int8ub,
+        "flags" / Int8ub,
+        "session" / Int16ub,
+        "key" / Int16ub,
+    )
+    result = format.parse(stream)
+    print(result)
 
-localIP     = "127.0.0.1"
-localPort   = 20001
-bufferSize  = 1024
-
-# Create a datagram socket
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-# Bind to address and ip
 UDPServerSocket.bind((localIP, localPort))
 
-print("Master server up and listening")
- 
+sessions = Sessions()
 
-# Listen for incoming datagrams
+print("Master server up and listening")
+
 while(True):
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
     message = bytesAddressPair[0]
     address = bytesAddressPair[1]
 
     process_message(message)
-    
 
     #clientMsg = "Message from Client:{}".format(message)
     clientIP  = "Client IP Address:{}".format(address)
